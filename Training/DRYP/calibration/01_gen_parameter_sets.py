76f6115235279d88d21566c173f4b21d36b83c8c
eaquichimbomiguitama <eaquichimbomiguitama@hpc-14.grit.ucsb.edu>
2026-07-28 22:19:32 +0000

    update MC generation files

--- Training/DRYP/calibration/01_gen_parameter_sets.py
+++ Training/DRYP/calibration/01_gen_parameter_sets.py
@@ -8,32 +8,51 @@ import os
 import pandas as pd
 import numpy as np
 
+GLOBAL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/"
+GLOBAL_PATH = "/home/chc-andres/AF/datasets/calibration/csv/"
 # Number of samples to generate (you can change this)
-n_samples = 100  
+n_samples = 10 
 
 # Read ranges of parameter file for model simulations
-fname = "/user/work/km19051/HAD_basin/calibration/HAD_parameter_range_file.csv"
-#fname = "C:/Users/km19051/OneDrive - Cardiff University/PhD/WS/LandLab/HAD/WS/input_model/HAD_parameter_range_file.csv"
+fname = GLOBAL_PATH + "AF_parameter_range_file.csv"
+##fname = "C:/Users/km19051/OneDrive - Cardiff University/PhD/WS/LandLab/HAD/WS/input_model/HAD_parameter_range_file.csv"
 
-# Read CSV file
-
-# Read CSV file
-df = pd.read_csv(fname)
+# check if the file exists
+if not os.path.exists(fname):
+	# create a new file with the specified ranges
+	df = pd.DataFrame({
+		"Grid": ["1km", "1km"],
+		"tstep": [1, 1],
+		"model": ["AF", "AF"],
+		"type": ["min", "max"],
+		"uz_kdroot": [1.0, 1.5],
+		"uz_kkast": [0.1, 0.5],
+		"riv_kksat": [0.2, 0.6],
+		"sz_kksat": [0.3, 0.7],
+		"sz_ksy": [0.5, 1.3],
+	})
+	df.to_csv(fname, index=False)
+	print(f"File {fname} not found. A new file has been created with default ranges. Please check and modify the ranges as needed.")
+else:
+	print(f"Found the file {fname}. Proceeding with parameter generation.")
+	# Read CSV file
+	df = pd.read_csv(fname)
 
 # Drop non-numeric / fixed columns
 fixed_cols = ["Grid", "tstep", "model", "type"]
 param_cols = [c for c in df.columns if c not in fixed_cols]
 
-# list of model to generate parameter sets
-label_model = ["JU",]
+# get list of model to generate parameter sets
+label_model = df["model"].unique()
 
+# loop through each model and generate parameter sets
 for ilabel_model in label_model:
 	
 	# Read CSV file
-	df = pd.read_csv(fname)
+	#df = pd.read_csv(fname)
 	
 	# path output
-	path_output = fname.split('.')[0] + '_' + ilabel_model + '_2.csv'
+	path_output = fname.split('.')[0] + '_' + 'sets.csv'
 
 	## input file
 	#df = df.groupby(['model'])#.reset_index()
--- Training/DRYP/calibration/03_DRYP_generate_spatial_parameter_sets.py
+++ Training/DRYP/calibration/03_DRYP_generate_spatial_parameter_sets.py
@@ -1,26 +1,44 @@
 import numpy as np
 import pandas as pd
+import os
 
 # generate a dataframe with n rows and m columns, where each column contains random numbers
 # between min and max. The columns should be named "col1", "col2", ..., "colm".
 # min and max values are provided as arrays of length m, where min[i] and
 # max[i] correspond to the minimum and maximum values for column i.
 
-n = 100
-m = 23
+GLOBAL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/"
+# GLOBAL_PATH = "/user/work/km19051/HAD_basin/calibration/"
+GLOBAL_PATH = "/home/chc-andres/AF/datasets/calibration/csv/"
 
-# The best and worst values are used to calculate the variance, which is then used to set the min and max values for each column. The first 7 columns have a wider range of values, while the remaining columns are set to 1.0.
-best_value = 0.359163
-worst_value = 0.392913
-variance = np.abs(best_value - worst_value)
+# specify the path to the parameter range file, it should contain the min and max values for each parameter.
+# The file should be a CSV file with m rows and 2 columns, where the first column contains the minimum values
+# and the second column contains the maximum values for each parameter.
+parameter_range_file = GLOBAL_PATH + "AF_parameter_range_spatial_file.csv"
 
-# Set the min and max values for each column based on the best value and variance. The first 7 columns have a wider range, while the remaining columns are set to 1.0.
-min_values = np.full(m, best_value - variance)
-max_values = np.full(m, best_value + variance)
+n = 10
+m = 3
 
-# Set the min and max values for the first 7 columns to be wider than the remaining columns, which are set to 1.0.
-min_values[7:] = best_value
-max_values[7:] = best_value
+if not os.path.exists(parameter_range_file):
+    # The best and worst values are used to calculate the variance, which is then used to set the min and max values for each column. The first 7 columns have a wider range of values, while the remaining columns are set to 1.0.
+    best_value = 0.359163
+    worst_value = 0.392913
+    variance = np.abs(best_value - worst_value)
+
+    # Set the min and max values for each column based on the best value and variance. The first 7 columns have a wider range, while the remaining columns are set to 1.0.
+    min_values = np.full(m, best_value - variance)
+    max_values = np.full(m, best_value + variance)
+
+    # Set the min and max values for the first 7 columns to be wider than the remaining columns, which are set to 1.0.
+    if m > 7:
+        min_values[7:] = best_value
+        max_values[7:] = best_value
+else:
+    # Read the parameter range file to get the min and max values for each column. The file should contain m rows and 2 columns, where the first column contains the minimum values and the second column contains the maximum values for each parameter.
+    df = pd.read_csv(parameter_range_file)
+    min_values = df.iloc[:, 0].values
+    max_values = df.iloc[:, 1].values
+    m = len(min_values)  # Update m based on the number of parameters in the file
 
 # Generate random data for each column based on the specified min and max values. The first 7 columns will have a wider range of values, while the remaining columns will be set to 1.0.
 data = np.random.rand(n, m) * (max_values - min_values) + min_values
@@ -33,5 +51,6 @@ factor = 1.0
 df = pd.DataFrame(data, columns=[f"col{i+1}" for i in range(m)])
 
 # Save the DataFrame to a CSV file named "random_data.csv". The CSV file will contain the generated random data, with column names as headers.
-path_spatial_paramters = "/user/work/km19051/HAD_basin/calibration/HAD_spatial_parameter_range_file_JU.csv"
+path_spatial_paramters = GLOBAL_PATH + "AF_spatial_parameter_sets_file.csv"
 df.to_csv(path_spatial_paramters, index=False)
+print(f"Random data saved to {path_spatial_paramters}.")
